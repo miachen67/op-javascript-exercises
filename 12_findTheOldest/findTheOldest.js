a8dbf3f4d8613778127e6b27ebbeaebea64c2d9d
miachen67 <miachen2005@gmail.com>
2024-03-22 12:08:51 -0700

    finished most exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    const date = new Date();
+    for (let i = 0; i < people.length; i++){
+        if (people[i].yearOfDeath == null){
+            people[i].age = date.getFullYear() - people[i].yearOfBirth;
+        } else {
+            people[i].age = people[i].yearOfDeath - people[i].yearOfBirth;
 
+        }
+        if (oldest.age < people[i].age){
+            oldest = people[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
